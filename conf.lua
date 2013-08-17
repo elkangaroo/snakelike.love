cdf17542088d830b7a4ef8e5a962c5ec20a0c541
Alexander Wenzel <elkangaroo@googlemail.com>
2013-08-17 10:48:57 +0200

    Fixed enquoting of website in conf.lua

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
   t.title = "a little bite snake löve"        -- The title of the window the game is in (string)
   t.author = "elkangaroo"     -- The author of the game (string)
-  t.url = https://github.com/elkangaroo/snakelike.love                 -- The website of the game (string)
+  t.url = "https://github.com/elkangaroo/snakelike.love"                 -- The website of the game (string)
   t.identity = nil            -- The name of the save directory (string)
   t.version = "0.8.0"         -- The LÖVE version this game was made for (string)
   t.console = false           -- Attach a console (boolean, Windows only)
